a971339f93c402b6444079f130f9a66703cd2d78
wzprogrammer <59797592+wzprogrammer@users.noreply.github.com>
2022-06-24 16:49:54 +0800

    Update sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1,5 +1,11 @@
 struct sched_entity {
-	int a;
+	long sum_exect_time;
+	long prev_sum_exect_time;
+	long start_exec_time;
+
+	long vruntime;
+	int weight;
+	int on_rq;
 };
 struct task_struct {
 	struct sched_entity se;
