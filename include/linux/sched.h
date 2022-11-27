91fb0db8d4037cd383c9c31ca593be5528a610e7
wzprogrammer <59797592+wzprogrammer@users.noreply.github.com>
2022-11-27 18:27:53 +0800

    Update sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1,12 +1,28 @@
+struct sched_avg {
+	unsigned long load_avg;
+	unsigned long runnable_avg;
+	unsigned long util_avg;
+};
 struct sched_entity {
-	long sum_exect_time;
-	long prev_sum_exect_time;
+	long sum_exec_time;
+	long prev_sum_exec_time;
 	long start_exec_time;
 
 	long vruntime;
 	int weight;
 	int on_rq;
+
+	struct sched_avg avg;
+};
+struct cfs_rq {
+	struct sched_avg avg;
+	struct sched_entity* curr;
 };
 struct task_struct {
 	struct sched_entity se;
+	struct cfs_rq* cfs_rq;
+};
+struct rq {
+	struct cfs_rq cfs_rq;
+	unsigned long cpu_capacity_orig;
 };
